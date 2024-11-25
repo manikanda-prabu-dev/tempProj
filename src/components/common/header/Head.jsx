68dff9804ab8a4ede8c48fd81439efb4ada3a2f7
Manikanda Prabu.A <a.manikanda96@gmail.com>
2024-11-25 11:29:36 +0530

    FEAT : Initial commit

---
+++ src/components/common/header/Head.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container flexSB'>
+          <div className='logo'>
+            <h1>CHEMMOZHI</h1>
+            <span>CANADA CHEMMOZHI ACADEMY</span>
+          </div>
+
+          <div className='social'>
+            <i className='fab fa-facebook-f icon'></i>
+            <i className='fab fa-instagram icon'></i>
+            <i className='fab fa-twitter icon'></i>
+            <i className='fab fa-youtube icon'></i>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import Heading from "../../common/heading/Heading";
+import "./Hero.css";
+
+const Hero = () => {
+  const paragraph =
+    "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.";
+  const words = paragraph.split(" ");
+
+  const [visibleWords, setVisibleWords] = useState([]);
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      if (currentIndex < words.length) {
+        setVisibleWords((prev) => [...prev, words[currentIndex]]);
+        currentIndex++;
+      } else {
+        clearInterval(interval); 
+      }
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <section className="hero">
+        <div className="container">
+          <div className="row">
+            <Heading subtitle="WELCOME TO CHEMMOZHI" title="Best Online Education Expertise" />
+            <p>{visibleWords.join(" ")}</p>
+            <div className="button">
+              <button className="primary-btn">
+                GET STARTED NOW <i className="fa fa-long-arrow-alt-right"></i>
+              </button>
+              <button>
+                VIEW COURSE <i className="fa fa-long-arrow-alt-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="margin"></div>
+    </>
+  );
+};
+
+export default Hero;
